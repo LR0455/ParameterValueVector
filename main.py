eb9829e35fbd3bbe7e9dd58bda7d77159c0c5284
liren <y0975422576@gmail.com>
2019-08-15 13:39:40 +0800

    Delete mapping_table and Add vector predict

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import threading
 # import self function
 import generate as gn
 import pvv_model as pvv
+#import test as pvv
 
 if __name__ == '__main__':
     print("get...")
 
-    data = gn.generate_data()
+    #data = gn.generate_data()
+    data = [{"key":"2", "pvv":[[1, 0.1, 0.05, 1],[2, 0.2, 0.1, 3],[3, 0.3, 0.15, 5],[4, 0.4, 0.2, 7],[5, 0.5, 0.25, 9],[6, 0.6, 0.3, 11],[7, 0.7, 0.35, 13],[8, 0.8, 0.4, 15]]}]
     
     pvv_model = pvv.ParameterValueVector(2, 1, 64, 2, 100, 3, 2048)
     pvv_model.pvv_model_train(data)
     
-    data = gn.generate_data()
+    #data = gn.generate_data()
+    data = [{"key":"2", "pvv":[[3, 0.3, 0.15, 5],[4, 0.4, 0.3, 7],[5, 1, 0.25, 10],[6, 0.6, 0.3, 11]]}]
+    
     pvv_model.pvv_model_predict(data)
     
--- pvv_model.py
+++ pvv_model.py
@@ -5,18 +5,15 @@ from tensorboardX import SummaryWriter
 from torch.utils.data import TensorDataset, DataLoader
 import argparse
 import os
-import json
-
-from sklearn.decomposition import PCA
 
 # LSTM model
 class Model(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_keys):
+    def __init__(self, input_size, hidden_size, num_layers, output_dim):
         super(Model, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, num_keys)
+        self.fc = nn.Linear(hidden_size, output_dim)
         
         # Device configuration
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -29,11 +26,8 @@ class Model(nn.Module):
         return out
 
 class ParameterValueVector():
-
-    # initial (2, 1, 64, 2, 100, 3, 2048)
     def __init__(self, window_size, input_size, hidden_size,
-                       num_layers, num_classes, num_epochs, batch_size):
-        
+                       num_layers, num_classes, num_epochs, batch_size):   
         # Device configuration
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -45,13 +39,7 @@ class ParameterValueVector():
         self.num_classes = num_classes
         self.num_epochs = num_epochs
         self.batch_size = batch_size
-
-        # Mapping table
-        self.table_kind = {}
-        self.table_vti = {}
-        self.table_itv = {}
-        
-        
+    
         # path
         self.model_dir = 'model'
         self.log = 'Adam_batch_size=' + str(self.batch_size) + ';epoch=' + str(self.num_epochs)
@@ -73,11 +61,9 @@ class ParameterValueVector():
         # test variable
         self.test = None
         self.predicted = None
-        self.threshold = 10
-        self.filter_test = None
+        self.threshold = 4
 
     def generate_tensor_dataset(self, pvv):
-    
         inputs = []
         outputs = []   
         window_size = self.window_size
@@ -85,104 +71,28 @@ class ParameterValueVector():
         for i in range(len(pvv) - window_size):
             inputs.append(pvv[i : i + window_size])
             outputs.append(pvv[i + window_size])
-    
-        # print("inputs : ", inputs)
-        # print("outputs : ", outputs)
 
         dataset = TensorDataset(torch.tensor(inputs, dtype=torch.float), torch.tensor(outputs))
         return dataset
     
-    def check_vector_in_table(self, vector, table):
-        for i in range(len(table)):
-            same = True
-            for j in range(len(vector)):
-                if vector[j] != table[i][j]:
-                    same = False
-                    break
-            if same == True:
-                return True
-        return False
-    
-    def save_mapping_table(self, filename):
-        # clear file
-        open(filename, 'w').close()
-
-        json_file = {}
-        for key in self.table_kind:
-            json_file[key] = {}
-            json_file[key]['table_kind'] = self.table_kind[key]
-            json_file[key]['table_vti'] = self.table_vti[key]
-            json_file[key]['table_itv'] = self.table_itv[key]
-            
-        with open(filename, 'w') as file:
-            json.dump(json_file, file)
-
-    def get_mapping_table(self, filename):
-        with open(filename) as file:
-            json_file = json.load(file)
-            
-            for key in json_file:
-                key_int = int(key)
-                self.table_kind[key_int] = json_file[key]['table_kind']
-                self.table_vti[key_int] = json_file[key]['table_vti']
-
-                self.table_itv[key_int] = {}
-                for key2 in json_file[key]['table_itv']:
-                    key2_int = int(key2)
-                    self.table_itv[key_int][key2_int] = json_file[key]['table_itv'][key2]
-               
-
-    def mapping_vector_to_int(self, key, data, test_flag):
-        if test_flag == False:
-            if key not in self.table_kind:
-                self.table_vti[key] = {}
-                self.table_itv[key] = {}
-                self.table_kind[key] = 0
-            # print("data : ", data)
-            for i in range(len(data)):
-                if self.check_vector_in_table(data[i], self.table_itv[key]) == True:
-                    data[i] = self.table_vti[key][str(data[i])]
-                else:
-                    self.table_itv[key][self.table_kind[key]] = data[i]
-                    self.table_vti[key][str(data[i])] = self.table_kind[key]
-                    data[i] = self.table_kind[key]
-                    self.table_kind[key] += 1
-            
-            # print("table_vti : ", self.table_vti[key])
-            # print("table_itv : ", self.table_itv[key])
-            # print("transform_data : ", data)
-        else:
-            for i in range(len(data)):
-                if self.check_vector_in_table(data[i], self.table_itv[key]) == True:
-                    data[i] = self.table_vti[key][str(data[i])]
-
-        return data
-
-    def preprocess(self, key, data, test_flag):
-        transform_data = self.mapping_vector_to_int(key, data, test_flag)
-        return transform_data
-    
     def model_preprocess(self, key, data):
-        
-        self.num_classes = self.table_kind[key]
+        self.num_classes = len(data[key][0])
+        self.input_size = len(data[key][0])
        
         self.model = Model(self.input_size, self.hidden_size, self.num_layers, self.num_classes).to(self.device)
-        
         self.seq_dataset = self.generate_tensor_dataset(data[key])
-
         self.dataloader = DataLoader(self.seq_dataset, batch_size = self.batch_size, pin_memory = True)
 
         # Loss and optimizer
-        self.criterion = nn.CrossEntropyLoss()
+        self.criterion = nn.MSELoss() 
         self.optimizer = optim.Adam(self.model.parameters())
     
-    def forward_and_backward(self, seq, label):
-                   
+    def forward_and_backward(self, seq, label):       
         # Forward pass
         seq = seq.clone().detach().view(-1, self.window_size, self.input_size).to(self.device)
         output = self.model(seq)
-        loss = self.criterion(output, label.to(self.device))
-
+        loss = self.criterion(output.float(), label.float().to(self.device))
+    
         # Backward and optimize
         self.optimizer.zero_grad()
         loss.backward()
@@ -216,91 +126,61 @@ class ParameterValueVector():
     
         data = self.key_merge(raw_data)
 
-        for key in data:
-            
-            self.num_classes = self.table_kind[key]
+        for key in data:           
+            self.num_classes = len(data[key][0])
+            self.input_size = len(data[key][0])
     
-            # print("log key : ", key, " predicting ...")
+            print("log key : ", key, " predicting ...")
             
             self.get_model(key)
             
-            print("data : ", data[key])
+            self.test = data[key]
 
-            self.test = self.preprocess(key, data[key], True)
-            
-            print("test : ", self.test)
-
-            self.filter_test = []
-            for i in range(len(self.test)):
-                if type(self.test[i]) is not list:
-                    self.filter_test.append(self.test[i])
-                
-            print("filter_test : ", self.filter_test)
-
+            self.criterion = nn.MSELoss()
+        
             # Test the model
             with torch.no_grad():
-                for i in range(len(self.filter_test) - self.window_size):
-                    
-                    seq = self.filter_test[i:i + self.window_size]
-                    label = self.filter_test[i + self.window_size]
+                for i in range(len(self.test) - self.window_size):
+                    seq = self.test[i : i + self.window_size]
+                    label = self.test[i + self.window_size]
                     
                     seq = torch.tensor(seq, dtype = torch.float).view(-1, self.window_size, self.input_size).to(self.device)
                     label = torch.tensor(label).view(-1).to(self.device)
-                    
-                    output = self.model(seq)
-                    predicted = torch.argsort(output, 1)[0][-1:]
-                    
-                    pdt_v = self.table_itv[key][int(predicted)]
-                    lbl_v = self.table_itv[key][int(label)] 
-                     
-                    SSE = 0
-                    for i in range(len(pdt_v)):
-                        SSE += (pdt_v[i] - lbl_v[i]) * (pdt_v[i] - lbl_v[i])
+                    predicted = self.model(seq).view(-1)
 
-                    if SSE >= self.threshold:
-                        print("log key : ", key, " ", lbl_v, " is anomaly")
+                    loss = self.criterion(predicted.float(), label.float().to(self.device))
+                    if loss.item() >= self.threshold:
+                        print("predicted :", predicted)
+                        print("label :", label)          
+                        print("loss :", loss.item())
+                        print("log key :", key, "->", label, "is anomaly")
      
-        #     print("log key : ", key, " predict finish")
+            print("log key :", key, "predict finish")
     
-        # print("Finished Predicting")
+        print("Finished Predicting")
     
     def pvv_model_train(self, raw_data):
 
-        if os.path.isfile('mapping_table.json'):
-            self.get_mapping_table('mapping_table.json')
-
-        # print("kind : ", self.table_kind)
-        # print("vti : ", self.table_vti)
-        # print("itv : ", self.table_itv)
-        
-
         data = self.key_merge(raw_data)
-
         self.data = data.copy()
 
         for key in data:
-
-            data[key] = self.preprocess(key, data[key], False)
-            
             self.model_preprocess(key, data)
 
             self.writer = SummaryWriter(logdir='log/' + self.log)
 
-            # print(str(key) + " training ...")
+            print(key, " training ...")
 
             for epoch in range(self.num_epochs):  # Loop over the dataset multiple times
-            # for epoch in range(1):
                 self.train_loss = 0
                 for (seq, label) in self.dataloader:
-                    # print("seq : ", seq)
                     self.forward_and_backward(seq, label)
                    
-                # print('Epoch [{}/{}], Train_loss: {:.4f}'.format(epoch + 1, self.num_epochs, self.train_loss / len(self.dataloader.dataset)))
+                print('Epoch [{}/{}], Train_loss: {:.4f}'.format(epoch + 1, self.num_epochs, self.train_loss / len(self.dataloader.dataset)))
                 self.writer.add_scalar('train_loss', self.train_loss / len(self.dataloader.dataset), epoch + 1)
             
             self.save_model(key)
-            self.save_mapping_table('mapping_table.json')
             self.writer.close()
             
-            print('Finished Training')    
+            print(key, ' Finished Training')    
         
